b7123c7cd380c5b13a3872b89300e45e09ae48f8
Alex Suilea <alexsuilea21@gmail.com>
2022-10-31 23:56:11 +0200

    Branch1 login

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host = environment.apiUrl;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+  }
 }
