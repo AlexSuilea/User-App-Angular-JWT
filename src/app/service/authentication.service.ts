65331a1f4132565ac01e9725dd848ed7097e635f
Alex Suilea <alexsuilea21@gmail.com>
2022-11-01 23:54:59 +0200

    Branch1 load token

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -9,10 +9,48 @@ import { User } from '../model/user';
 })
 export class AuthenticationService {
   private host = environment.apiUrl;
+  private token: any;
+  private loggedInUsername: any;
   
   constructor(private http: HttpClient) { }
 
   public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
-    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>
+    (`${this.host}/user/login`, user, {observe: 'response'});
   }
+
+  public register(user: User): Observable<User| HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>
+    (`${this.host}/user/register`, user);
+  }
+
+  public logOut(): void {
+    this.token = null;
+    this.loggedInUsername = null;
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('users');
+  }
+
+  public saveToken(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  public addUserToLocalCache(user: User): void {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUserFromLocalCache(): User {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  public loadToken(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  public getToken(): string {
+    return this.token;
+  }
+
 }
