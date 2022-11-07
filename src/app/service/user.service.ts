cd81171df06e1a20bc7d87b9cad4d3ba32281d45
Alex Suilea <alexsuilea21@gmail.com>
2022-11-08 00:01:08 +0200

    Branch userservice calls

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http'
+import { HttpClient, HttpResponse, HttpErrorResponse, HttpEvent } from '@angular/common/http'
 import { environment } from '../../environments/environment'
 import { Observable, subscribeOn } from 'rxjs';
 import { User } from '../model/user';
@@ -15,4 +15,53 @@ export class UserService {
   public getUsers(): Observable<User[] | HttpErrorResponse> {
     return this.http.get<User[]>(`${this.host}/user/list`);
   }
+
+  public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/update`, formData);
+  }
+
+  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get(`${this.host}/user/resetPassword/${email}`);
+  }
+
+  public updateProfileImage(formData: FormData): Observable<HttpEvent<User> | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/user/updateProfileImage`, formData,
+    {reportProgress: true,
+      observe: 'events'
+    });
+  }
+
+  public deleteUser(userId: number): Observable<any | HttpErrorResponse> {
+    return this.http.delete<User>(`${this.host}/user/delete/${userId}`);
+  }
+
+  public addUserToLocalCache(users: User[]): void {
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  public getUserToLocalCache(): User[] {
+    if(localStorage.getItem('users')){
+      return JSON.parse(localStorage.getItem('users')!);
+    }
+    return null!;
+  }
+
+  public createUserFormDate(loggedInUsername: string, user: User, profileImage: File): FormData {
+    const formData =  new FormData();
+    formData.append('currentUsername', loggedInUsername);
+    formData.append('firstName', user.firstName);
+    formData.append('lastName', user.lastName);
+    formData.append('username', user.username);
+    formData.append('email', user.email);
+    formData.append('role', user.role);
+    formData.append('profileImage', profileImage);
+    formData.append('isActive', JSON.stringify(user.active));
+    formData.append('isNonLocked', JSON.stringify(user.notLocked));
+    return formData;
+  }
+
 }
